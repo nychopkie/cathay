3f8607e6d409ed7cb61fc77627985df29f0b7882
meng <gordonmeng2021@gmail.com>
2022-11-19 12:24:10 +0800

    nice

--- velzon/models.py
+++
@@ -1,8 +0,0 @@
-from flask_login import UserMixin
-from . import db
-
-class User(db.Model,UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(100),unique=True)
-    password = db.Column(db.String(100))
-    username = db.Column(db.String(100),unique=True)
