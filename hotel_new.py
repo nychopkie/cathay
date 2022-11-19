57a16f81d13e1b337b51469f8b036ef1c03d6936
biajibiaji233 <56726150+biajibiaji233@users.noreply.github.com>
2022-11-20 07:47:43 +0800

    Add files via upload

--- hotel_new.py
+++ hotel_new.py
@@ -6,51 +6,56 @@ import datetime
 import time
 from selenium.webdriver.chrome.options import Options
 
-chrome_opt = Options()      # keep it open
-chrome_opt.add_experimental_option('detach',True)
+class Booking:
+    def __init__(self):
+        self.go_book()
 
-district = 'Akihabara'      #assuemd we get this result from previous operation
-first_click = False     #loop controler
+    def go_book(self):
+        chrome_opt = Options()      # keep it open
+        chrome_opt.add_experimental_option('detach',True)
 
-path = Service('chromedriver.exe')
-driver = webdriver.Chrome(service=path, chrome_options=chrome_opt)
+        district = 'Akihabara'      #assuemd we get this result from previous operation
+        first_click = False     #loop controler
 
-driver.get('https://www.agoda.com/?cid=1841704')
+        path = Service('chromedriver.exe')
+        driver = webdriver.Chrome(service=path, chrome_options=chrome_opt)
 
-# search for hotels in this district
-search = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[1]/div/div[2]/div/div/div[1]/div/div/input')
-search.send_keys(district)
+        driver.get('https://www.agoda.com/?cid=1841704')
 
-# open date selection panel
-dates = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]')
-dates.click()
+        # search for hotels in this district
+        search = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[1]/div/div[2]/div/div/div[1]/div/div/input')
+        search.send_keys(district)
 
-#assumption
-dep_date = datetime.datetime(year=2022,month=11,day=21)
-return_date = datetime.datetime(year=2022,month=11,day=23)
+        # open date selection panel
+        dates = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]')
+        dates.click()
 
-# select date on panel
-dates_list = driver.find_elements(By.CLASS_NAME, 'DayPicker-Day__label')
-for this_date in dates_list:
-    if this_date.text == dep_date.day and first_click == False:
-        this_date.click()
-        first_click = True
-    elif this_date.text == return_date.day and first_click == True:
-        this_date.click()
+        #assumption
+        dep_date = datetime.datetime(year=2022,month=11,day=21)
+        return_date = datetime.datetime(year=2022,month=11,day=23)
 
-# search buttom
-search_btm = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[2]/button')
-search_btm.click()
-time.sleep(5)
+        # select date on panel
+        dates_list = driver.find_elements(By.CLASS_NAME, 'DayPicker-Day__label')
+        for this_date in dates_list:
+            if this_date.text == dep_date.day and first_click == False:
+                this_date.click()
+                first_click = True
+            elif this_date.text == return_date.day and first_click == True:
+                this_date.click()
 
-#find first star ie most start filter
-most_stars = driver.find_element(By.CLASS_NAME, 'filter-item-react')  #first element
-most_stars.click()
-time.sleep(3)
+        # search buttom
+        search_btm = driver.find_element(By.XPATH, '//*[@id="SearchBoxContainer"]/div[2]/button')
+        search_btm.click()
+        time.sleep(5)
 
-# click into first suggested hotel
-recommand = driver.find_element(By.CLASS_NAME, 'PropertyCard__HotelName') #first hotel
-recommand.click()
+        #find first star ie most start filter
+        most_stars = driver.find_element(By.CLASS_NAME, 'filter-item-react')  #first element
+        most_stars.click()
+        time.sleep(3)
+
+        # click into first suggested hotel
+        recommand = driver.find_element(By.CLASS_NAME, 'PropertyCard__HotelName') #first hotel
+        recommand.click()
 
 
-# tell them do it their own
+        # tell them do it their own
