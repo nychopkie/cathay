2a08e465680bc57d1578ab4f524c587dd6dffa1a
biajibiaji233 <56726150+biajibiaji233@users.noreply.github.com>
2022-11-19 21:22:57 +0800

    Add files via upload

--- buy_ticket.py
+++ buy_ticket.py
@@ -8,50 +8,56 @@ from selenium.webdriver.common.keys import Keys
 from get_flight_data import FlightSearch
 
 flight_info = FlightSearch()
-month_dict = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", \
-    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
+month_dict = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
+              7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
 
-path ='chromedriver.exe'
-driver = webdriver.Chrome(service=path)
+path =Service('chromedriver.exe')
 
-driver.get('https://www.cathaypacific.com/cx/en_HK/book-a-trip.html')
+class buy_ticket:
+    def __init__(self):
+        pass
 
-start_port = driver.find_element(By.XPATH, '//*[@id="depart-label"]')  # departure airport choice
-start_port.send_keys(flight_info.start)
-start_port.send_keys(Keys.ENTER)
+    def buy(self):
+        driver = webdriver.Chrome(service=path)
 
-end_port = driver.find_element(By.XPATH, '//*[@id="destination-label"]')  # destination airport choice
-end_port.send_keys(flight_info.dest)
-end_port.send_keys(Keys.ENTER)
+        driver.get('https://www.cathaypacific.com/cx/en_HK/book-a-trip.html')
 
-dep_date = driver.find_element(By.XPATH, '//*[@id="dpuxzabv"]/span[2]/span[3]')  #departure date choice
-dep_date.click()
+        start_port = driver.find_element(By.XPATH, '//*[@id="depart-label"]')  # departure airport choice
+        start_port.send_keys(flight_info.start)
+        start_port.send_keys(Keys.ENTER)
 
-# first_month = driver.find_element(By.XPATH, '//*[@id="dp1668851740859"]/div/div[1]/table/caption/span[1]')  # calender front part month
-# second_month = driver.find_element(By.XPATH, '//*[@id="dp1668851740859"]/div/div[2]/table/caption/span[1]')  #calender last part month
+        end_port = driver.find_element(By.XPATH, '//*[@id="destination-label"]')  # destination airport choice
+        end_port.send_keys(flight_info.dest)
+        end_port.send_keys(Keys.ENTER)
 
+        dep_date = driver.find_element(By.XPATH, '//*[@id="dpuxzabv"]/span[2]/span[3]')  #departure date choice
+        dep_date.click()
 
-# if first_month.text == month_dict[ flight_info.date.month]:  # departure date in first month choice
-
-calendar_dep_day = driver.find_elements(By.CLASS_NAME, 'ui-state-default')[::-1] # inverse to make sure it's valid even when it go to second month
-departure_day = flight_info.date.day
-for ele in calendar_dep_day:
-    if ele.text == departure_day:
-        ele.click()
+        # first_month = driver.find_element(By.XPATH, '//*[@id="dp1668851740859"]/div/div[1]/table/caption/span[1]')  # calender front part month
+        # second_month = driver.find_element(By.XPATH, '//*[@id="dp1668851740859"]/div/div[2]/table/caption/span[1]')  #calender last part month
 
 
+        # if first_month.text == month_dict[ flight_info.date.month]:  # departure date in first month choice
+
+        calendar_dep_day = driver.find_elements(By.CLASS_NAME, 'ui-state-default')[::-1] # inverse to make sure it's valid even when it go to second month
+        departure_day = flight_info.date.day
+        for ele in calendar_dep_day:
+            if ele.text == departure_day:
+                ele.click()
 
 
-re_day = driver.find_element(By.XPATH, '//*[@id="dppdvs3"]/span[2]/span[3]')  # return date choice
-re_day.click()
+        re_day = driver.find_element(By.XPATH, '//*[@id="dppdvs3"]/span[2]/span[3]')  # return date choice
+        re_day.click()
 
-calendar_re_day = driver.find_elements(By.CLASS_NAME, 'ui-state-default')[::-1] # inverse to make sure it's valid even when it go to second month
-play_duration = datetime.timedelta(days=3)
-return_day = (flight_info.date + play_duration).day
-for ele in calendar_re_day:
-    if ele.text == return_day:
-        ele.click()
+        calendar_re_day = driver.find_elements(By.CLASS_NAME, 'ui-state-default')[::-1] # inverse to make sure it's valid even when it go to second month
+        play_duration = datetime.timedelta(days=3)
+        return_day = (flight_info.date + play_duration).day
+        for ele in calendar_re_day:
+            if ele.text == return_day:
+                ele.click()
 
+        submit_btm = driver.find_element(By.XPATH, '//*[@id="book-trip-flight"]/div[1]/form/button')
+        submit_btm.click()
 
 
 
