e640e012b290eab8ca551064d8e700d50c6954ed
biajibiaji233 <56726150+biajibiaji233@users.noreply.github.com>
2022-11-19 15:57:47 +0800

    Add files via upload

---
+++ get_flight_data.py
@@ -0,0 +1,40 @@
+import requests
+import datetime
+
+current_port = 'HKG'
+dest_port = 'BKK'
+utc_time = datetime.datetime.utcnow()
+
+url_head = 'https://cathayhackathon'
+url = f'{url_head}/operation/flightInformation/v3/next24hraircraftstatus'
+
+header = {
+    "X-applicationName": 'CUSS',
+    'trace_id': 'abcdefg',
+    'apikey': 'PGWzTIo4jAVwYioHCJL5F3ysiw8wLEDL'
+}
+
+para = {
+    'aircraftType': '',
+    'selectedAircraftType': '',
+    'aircraftRegistration': '',
+    'station': '',
+    'currentDateTimeUTC': utc_time.split()[1].split('.')[0],
+}
+class FlightSearch:
+    def __init__(self):
+        self.flight = {}
+        self.dept_time = {}
+        self.arri_time = {}
+        self.get_flight()
+
+    def get_flight(self):
+        response = requests.get(url=url)
+        response = response.json()
+        i = 0
+        for ele in response['flights']:
+            if ele['scheduleDepartureAirport'] == current_port and ele['scheduleArrivalAirport'] == dest_port:
+                self.flight[i] = f"{ele['airline']}{ele['flightNumber']}"
+                self.dept_time[i] = ele['takeoffTimeActualLocal'].split()[1]
+                self.arri_time[i] = ele['touchdownTimeActualLocal'].split()[1]
+                i += 1
