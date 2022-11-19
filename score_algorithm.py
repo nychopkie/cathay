1479409aac02ebcc9be1e1e38765661f36756243
gordon-cpl <changordon03@gmail.com>
2022-11-20 04:57:19 +0800

    latest_score_algo+meng_code

--- score_algorithm.py
+++ score_algorithm.py
@@ -1,8 +1,65 @@
+import requests
+import json
+import re
+import geopandas
+import geopy
+from geopy.geocoders import Nominatim
+
+class Getting_Best_Location():
+    # initial url
+    def __init__(self,origin_addresses,destination_addresses):
+        self.apikey="AIzaSyCJP88gPak3FtJq-ooijGdOEMa_MuknkLU"
+        self.url1="https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+        
+        # getting latitude of the origin location
+        geolocator = Nominatim(user_agent="my_request")
+        location1 = geolocator.geocode(origin_addresses)
+        self.origin_addresses=str(location1.latitude)+"%2C"+str(location1.longitude)
+        
+        # getting latitude of the destination location
+        self.url2="&destinations="
+        location2 = geolocator.geocode(destination_addresses)
+        self.destination_addresses=str(location2.latitude)+"%2C"+str(location2.longitude)
+
+        self.url3="&mode=driving&key="
+        self.fullurl=""
+        
+    def valid_location(self):
+        self.fullurl=self.url1+self.origin_addresses+self.url2+self.destination_addresses+self.url3+self.apikey
+        # getting distance adn durationg bewteen 2 places using API
+        output= requests.get(self.fullurl).json()
+        print(output)
+        
+        for obj in output["rows"]:
+            for data in obj["elements"]:
+                    # getting the distance and driving duration bewteen the 2 places
+                    dis= data["distance"]["text"]
+                    new_dis = int(re.sub("\skm", "", dis))
+                    dur =(data["duration"]["text"])
+                    new_dur = re.split("\s", dur)
+                    compare_dur=0
+                    # converting hr to mins
+                    if len(new_dur) ==4:
+                        compare_dur=int(new_dur[0])*60+int(new_dur[2])
+                    elif len(new_dur)==2:
+                        compare_dur=int(new_dur[0])
+                    # if the driving time >120 mins , it is considered as to far, it is not a feasible destination
+                    if compare_dur < 120:
+                        print(str(new_dis)+"km")
+                        print(str(compare_dur)+"mins")
+                        return True
+                    else:
+                        print(str(new_dis)+"km")
+                        print(str(compare_dur)+"mins")
+                        return False
+
 
 #https://en.wikipedia.org/wiki/Category:Tourist_attractions
 # We determine the duration of staying in each destination by its category (destination_type), 
-# we reference these data from wikipedia, but we only use some of them in our prototype
+# we reference these data from wikipedia, but we only use the relavent ones in our prototype
 category_duration = {"Museums":2, "Entertainment districts":6, "Entertainment events":2, "Shopping malls":3, "Scenic viewpoints":2}
+
+#class for destinations 
 class destinations:
     def __init__(self, name,out_or_in, popular_or_local, people_going_with_type, budget_used, destination_type):
         self.name = name
@@ -12,10 +69,10 @@ class destinations:
         self.budget_used = budget_used
         self.destination_type = destination_type
         self.duration = category_duration[destination_type]
-
+ 
 #sample destinations
-#assumptions:
-#only 3 types of travellers: Friends, Family, Couples
+#assumptions in our prototype:
+#only 3 types of travellers: Friends, Family, Couples, Individual
 #Only 4 natures of destination: Play, Shopping, Nature, Culture
 disney = destinations(name="Tokyo Disneyland", out_or_in="out", popular_or_local="popular",people_going_with_type= ["Friends","Family","Couples"],
                         budget_used= 500, destination_type="Entertainment districts")
@@ -23,30 +80,28 @@ disney = destinations(name="Tokyo Disneyland", out_or_in="out", popular_or_local
 aqua_city = destinations(name="Aqua City Odaiba", out_or_in="out", popular_or_local="popular",people_going_with_type= ["Family","Couples"],
                         budget_used= 300, destination_type="Shopping malls")
 
-universal_studios = destinations(name="Tokyo Universal Studios", out_or_in="out", popular_or_local="popular",people_going_with_type= ["Friends","Family","Couples"],
-                        budget_used= 500, destination_type="Entertainment districts")
-
-mount_fuji = destinations(name="Mount Fuji", out_or_in="out", popular_or_local="popular",people_going_with_type= ["Couples"],
+mount_fuji = destinations(name="Mount Fuji", out_or_in="out", popular_or_local="popular",people_going_with_type= ["Couples","Individual"],
                          budget_used= 100, destination_type="Scenic viewpoints")
 
 funabashi_andersen_park = destinations(name="Funabashi Andersen Park", out_or_in="out", popular_or_local="local",people_going_with_type= ["Couples","Family"],
                         budget_used= 50, destination_type="Scenic viewpoints")
 
-edo_museum = destinations(name="Edo-Tokyo Open Air Architectural Museum", out_or_in="in", popular_or_local="local",people_going_with_type= ["Couples","Family"],
+edo_museum = destinations(name="Edo-Tokyo Open Air Architectural Museum", out_or_in="in", popular_or_local="local",people_going_with_type= ["Couples","Family", "Individual"],
                         budget_used= 100, destination_type="Museums")
 
-tokyo_museum = destinations(name="Tokyo National Museum", out_or_in="in", popular_or_local="popular",people_going_with_type= ["Couples","Family"],
+tokyo_museum = destinations(name="Tokyo National Museum", out_or_in="in", popular_or_local="popular",people_going_with_type= ["Couples","Family", "Individual"],
                         budget_used= 100, destination_type="Museums")
 
 ginza = destinations(name="Ginza", out_or_in="in", popular_or_local="popular",people_going_with_type= ["Couples","Friends","Family"],
                         budget_used= 500, destination_type="Shopping malls")
 
-akihabara = destinations(name="Akihabara", out_or_in="in", popular_or_local="popular",people_going_with_type= ["Friends"],
-                        budget_used= 500, destination_type="Shopping malls")
+akihabara = destinations(name="Akihabara", out_or_in="in", popular_or_local="popular",people_going_with_type= ["Friends", "Family", "Individual"],
+                        budget_used= 400, destination_type="Shopping malls")
 
 Mannen_onsen = destinations(name="Mannen-yu onsen", out_or_in="in", popular_or_local="local",people_going_with_type= ["Couples"],
                         budget_used= 200, destination_type="Entertainment events")
 
+#class for travellers 
 class travel_input:
     def __init__(self, know_destination, destination, duration, budget_per_day, people_going_with, indoor, popular_locations, trip_focus):
         self.know_destination = know_destination
@@ -59,28 +114,29 @@ class travel_input:
         self.trip_focus = trip_focus
         self.total_budget = duration*budget_per_day
 
-    # #Sample input
-    # sample = travel_input(know_destination=True, destination="Tokyo", duration=3,budget_per_day=2000, 
-    #         people_going_with="Family", indoor=6, popular_locations= 8,trip_focus= {"Shopping":4,"Entertainment":3,"Nature":2,"Culture":1})
-
+    #generating the itinerary by calculating scores and filtering
     def generate_itinerary(self):
-        
-        all_destinations = [disney, universal_studios, mount_fuji, aqua_city, funabashi_andersen_park, edo_museum, tokyo_museum, ginza, akihabara, Mannen_onsen]
+        all_destinations = [disney, mount_fuji, aqua_city, funabashi_andersen_park, edo_museum, tokyo_museum, ginza, akihabara, Mannen_onsen]
         #score calulation
         changing_budget = self.total_budget
         DURATION_EACH_DAY = 10
         MAX_DESTINATION_PER_DAY = 2
-        num_destinations = self.duration*MAX_DESTINATION_PER_DAY #two destinations per day
+        MAX_BUDGET_PER_DEST = 1000
+        num_destinations = self.duration*MAX_DESTINATION_PER_DAY 
         destination_points = {}
+        HOTEL = "Tokyo Bay Shiomi Prince Hotel, 2 Chome-8-16 潮見江東區東京都日本" #hotel based on users choice
 
+        #We use the following algorithm to calculate the total score
+        #
         for i in range(len(all_destinations)):
-            if (all_destinations[i].budget_used > changing_budget):
+            if (all_destinations[i].budget_used > changing_budget #over-budget then swap to another location 
+                and Getting_Best_Location(all_destinations[i].name, HOTEL).valid_location()): #make sure the time distance between the destination and hotel < 2 hours
                 continue
             else:
-                if (all_destinations[i].people_going_with_type == self.people_going_with): #people going with
-                    people_going_with_score = 10
+                if (all_destinations[i].people_going_with_type == self.people_going_with): #types of travellers
+                    people_going_with_score = 10 #for making the maximum total score be 10
                 else:
-                    people_going_with_score = 3 #because 0.4*3 - 1.2 > 1 (minimum sum of three other factors)
+                    people_going_with_score = 3 #because 0.4*3 - 1.2 > 1 (minimum sum of three other factors -> dominate other choices in the worst case)
 
                 #trip focus
                 if (all_destinations[i].destination_type == "Museums"):
@@ -102,7 +158,9 @@ class travel_input:
                 elif (all_destinations[i].out_or_in == "local"):
                     popular_or_local_score = 10 - self.popular_locations
                 
-                total_score =  0.4 * people_going_with_score + 0.3 * (2.5*trip_focus_score) + 0.15 * out_or_in_score + 0.15 * popular_or_local_score
+                budget_score = 10* (1- all_destinations[i].budget_used/MAX_BUDGET_PER_DEST) #the lower the budget used the better
+
+                total_score =  0.4 * people_going_with_score + 0.3 * (2.5*trip_focus_score) + 0.1 * out_or_in_score + 0.1 * popular_or_local_score + 0.1* budget_score
 
                 destination_points[all_destinations[i].name] = total_score
 
@@ -114,42 +172,41 @@ class travel_input:
                     destination_points[key] = 0
                     confirmed_destination.append(key)
                     break
+        
 
 
         itinerary_destinations = {}
         copy_of_confirmed_destination = confirmed_destination
-
+        varying_len_confirmed_destination = len(copy_of_confirmed_destination)
+        print(copy_of_confirmed_destination)
 
         for i in range(1,self.duration+1):
             this_day_duration = 0
             destination_count = 0
-            varying_len_confirmed_destination = len(copy_of_confirmed_destination)
             itinerary_destinations["day" + str(i)] = []
-
-            for j in range(varying_len_confirmed_destination):
-                if (this_day_duration >= DURATION_EACH_DAY or destination_count >= 2):
+     
+            while (destination_count < MAX_DESTINATION_PER_DAY):
+                # assign attractions for every day
+                if (varying_len_confirmed_destination == 0):
                     break
 
-            # assign attractions for every day 
-            for i in range(1,self.duration+1):
-                itinerary_destinations["day" + str(i)]=[]
-                for j in range(MAX_DESTINATION_PER_DAY):
-                    if i==1 and j==0:
-                        itinerary_destinations["day" + str(i)].append(copy_of_confirmed_destination[0])
-                    else:    
-                        itinerary_destinations["day" + str(i)].append(copy_of_confirmed_destination[i+j*2])
-
-        return itinerary_destinations
-
-
-
-
+                for k in range(len(all_destinations)):
+                        assigned_location = copy_of_confirmed_destination[0]
+                        if (all_destinations[k].name == assigned_location):
+                            if (this_day_duration + all_destinations[k].duration) <= DURATION_EACH_DAY:
+                                this_day_duration += all_destinations[k].duration
+                                copy_of_confirmed_destination.remove(assigned_location)
+                                itinerary_destinations["day" + str(i)].append(assigned_location)
+                                varying_len_confirmed_destination -= 1
+                                destination_count += 1
+                                break
+             
+        return itinerary_destinations #return itinerary planned
 
 
-#run 
-#disney vs universal
-#distance formula
 
+
+#run with sample input
 sample = travel_input(know_destination=True, destination="Tokyo", duration=3,budget_per_day=2000, 
                     people_going_with="Family", indoor=6, popular_locations= 8,trip_focus= {"Shopping":4,"Entertainment":3,"Nature":2,"Culture":1})
             
