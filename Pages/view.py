01321272c7837de44615ea560362c3710e20c0c3
nychopkie <annie030103@gmail.com>
2022-11-20 00:47:16 +0800

    help

--- Pages/view.py
+++ Pages/view.py
@@ -1,14 +1,34 @@
 #Contains the routings and the view functions
 import json
-from random import randint
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request
 
 app = Flask(__name__)
 # app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
-@app.route("/home/")
+@app.route("/home")
 def home():
     return render_template("Home.html")
+
+@app.route("/Q1")
+def location():
+    return render_template("Location.html")
+
+@app.route("/Q1b")
+def chooselocation():
+    return render_template("chooselocation.html")
+
+@app.route("/Q2")
+def duration():
+    return render_template("duration.html")
+
+@app.route("/Q3")
+def budget():
+    return render_template("budget.html")
+
+@app.route("/Q4")
+def accompany():
+    return render_template("Accompany.html")
+
 
